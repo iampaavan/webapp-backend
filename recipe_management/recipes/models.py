da02998b44293731c343fca62f97c3dda9389880
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-02-01 16:59:55 -0500

    added user model

--- recipe_management/recipes/models.py
+++ recipe_management/recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import validate_email
+import uuid
 
-# Create your models here.
+class user(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=256)
+    last_name = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    email_address = models.EmailField(unique=True)
+    account_created = models.DateTimeField(auto_now_add=True)
+    account_updated = models.DateTimeField(auto_now=True)
+
