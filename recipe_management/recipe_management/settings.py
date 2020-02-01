33334d3e14e4ca2dd0f7420fa64ecab542ce6373
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-02-01 15:50:33 -0500

    added database connection

--- recipe_management/recipe_management/settings.py
+++ recipe_management/recipe_management/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'recipe_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipe',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1'
     }
 }
 
