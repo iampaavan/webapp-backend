92cac9668c5881f0f169cdca09e1ddad7f8731b6
iampaavan <gopalareddy.p@husky.neu.edu>
2020-02-01 18:25:48 -0500

    untracked files and models.py changes

--- recipe_management/recipes/models.py
+++ recipe_management/recipes/models.py
@@ -4,6 +4,7 @@ import uuid
 
 
 class User(models.Model):
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=256)
     last_name = models.CharField(max_length=256)
