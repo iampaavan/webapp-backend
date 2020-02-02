1a4ec09101308760bd62e06a3db9cd6f952f96cf
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-02-02 13:44:47 -0500

    update user endpoint complete and tested

--- recipe_management/recipes/urls.py
+++ recipe_management/recipes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('user', views.user),
+    path('user/self', views.update_user)
 ]
--- recipe_management/recipes/views.py
+++ recipe_management/recipes/views.py
@@ -15,6 +15,8 @@ email = ""
 def user(request):
     if request.method == 'POST':
         request_body = json.loads(request.body)
+        if not request_body:
+            return JsonResponse("No body Provided", status=204, safe=False)
         required_params = ['first_name', 'last_name', 'password', 'email_address']
         keys = request_body.keys()
         missing_keys = []
@@ -54,40 +56,58 @@ def update_user(request):
         auth = request.headers.get('Authorization')
         auth_status = checkauth(auth)
         request_body = json.loads(request.body)
+        if not request_body:
+            return JsonResponse("No body Provided", status=204, safe=False)
         if auth_status == 'success':
-            # allowed_params = ['first_name', 'last_name', 'password']
+            required_params = ['first_name', 'last_name', 'password', 'email_address']
+            keys = request_body.keys()
+            missing_keys = []
+            for item in required_params:
+                if item not in keys:
+                    missing_keys.append(item)
+            if missing_keys:
+                return HttpResponse("Missing {}".format(", ".join(missing_keys)), status=400,
+                                    content_type="application/json")
+
             user_obj = User.objects.get(email_address=email)
-            for item in request_body.keys:
-                if item == 'first_name':
-                    user_obj.first_name = request_body['first_name']
-                if item == 'last_name':
+            changed = False
+            for item in request_body.keys():
+                if item == 'first_name' and user_obj.first_name != request_body['first_name']:
+                    user_obj.first_name = request_body[item]
+                    changed = True
+                    continue
+                elif item == 'last_name' and user_obj.last_name != request_body['last_name']:
                     user_obj.last_name = request_body['last_name']
-                if item == 'password':
-                    user_obj.password == request_body['password']
-                else:
-                    return HttpResponse("{} cannot be updated".format(item), status=400)
-            user_obj.save()
-            return HttpResponse("Successfully updated User", status=200)
+                    changed = True
+                    continue
+                elif item == 'password' and not (decryptpwd(request_body['password'].encode('utf-8'), user_obj.password)):
+                    encrypted_pwd = encryptpwd(request_body['password'])
+                    user_obj.password = encrypted_pwd
+                    changed = True
+                    continue
+                elif request_body['email_address'] != user_obj.email_address:
+                    return HttpResponse("Email address cannot be updated", status=400)
+            if changed:
+                ser = UserSerializer(user_obj)
+                user_obj.save()
+                return JsonResponse(ser.data, status=200)
+            else:
+                return JsonResponse("No changes to update", status=200, safe=False)
+        elif auth_status == "wrong_pwd":
+            return JsonResponse("Wrong Password", status=403, safe=False)
+        elif auth_status == "no_user":
+            return JsonResponse("User Not Found", status=404, safe=False)
     else:
         return JsonResponse("Invalid request method", status=400, safe=False)
-
-
-
-
-
-
-
-
-
 
 def encryptpwd(pwd):
     salt = bcrypt.gensalt()
     encoded_pwd = pwd.encode('utf-8')
-    hash_pwd = bcrypt.hashpw(encoded_pwd, salt)
+    hash_pwd = bcrypt.hashpw(encoded_pwd, salt).decode('utf-8')
     return hash_pwd
 
 def decryptpwd(pwd, hashed_pwd):
-    return bcrypt.checkpw(pwd, hashed_pwd)
+    return bcrypt.checkpw(pwd, hashed_pwd.encode('utf-8'))
 
 def checkauth(auth):
     global email
@@ -98,8 +118,8 @@ def checkauth(auth):
     email = creds[0]
     pwd = creds[1]
     if (User.objects.filter(email_address=email).exists()):
-        user_obj = user.objects.get(email_address=email)
-        if (decryptpwd(pwd,user_obj.password)):
+        user_obj = User.objects.get(email_address=email)
+        if (decryptpwd(pwd.encode('utf-8'),user_obj.password)):
             return "success"
         else:
             return "wrong_pwd"
