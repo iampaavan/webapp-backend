d0c1e58a500dbdb64b28b925a143985da971967f
Hemal Gadhiya <gadhiya.h@husky.neu.edu>
2020-02-15 22:36:41 -0500

    fix

--- k8s/change_imagetag.sh
+++ k8s/change_imagetag.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-read -p "Enter Your Username: "  username
-
-read -p "Enter Your Image Name: "  image
-
 rev=$(git rev-parse HEAD)
 
-sed -i -e "s/username/$username/g" -e "s/clock/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" backend-k8s-ReplicaSet.yaml
+sed -i -e "s/username/$1/g" -e "s/clock/$2/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" ~/csye7374/webapp-backend/k8s/backend-k8s-ReplicaSet.yaml
 
-sed -i -e "s/username/$username/g" -e "s/clock/$image/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" backend-k8s-job.yaml
+sed -i -e "s/username/$1/g" -e "s/clock/$2/g" -e "s/81750055651bbe6db78ac1828abd43144f08213e/$rev/g" ~/csye7374/webapp-backend/k8s/backend-k8s-job.yaml
 
 cd
 
@@ -19,8 +15,7 @@ base64=$(base64 config.json | tr -d \\n)
 cd
 
 cd ~/csye7374/webapp-backend/k8s
-sed -i "s/secret/$base64/g" backend-k8s-secrets.yaml
 
-read -p "Enter Your RDS_URL: "  RDS_URL
+sed -i "s/secret/$base64/g" ~/csye7374/webapp-backend/k8s/backend-k8s-secrets.yaml
 
-sed -i "s/RDS_URL/$RDS_URL/g" backend-k8s-configMap.yaml
+sed -i "s/RDS_URL/$3/g" backend-k8s-configMap.yaml
