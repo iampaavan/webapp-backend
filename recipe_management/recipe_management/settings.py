18ba32b25d35335e085867e3ed81f2de89bf9e81
iampaavan <gopalareddy.p@husky.neu.edu>
2020-03-10 11:06:30 -0400

    settings

--- recipe_management/recipe_management/settings.py
+++ recipe_management/recipe_management/settings.py
@@ -116,8 +116,8 @@ REDIS_CONN_POOL_1 = redis.ConnectionPool(host=os.environ.get("redisHost"), port=
 #         'PORT': "5432",
 #     }
 # }
-
-
+#
+#
 # CACHES = {
 #     "default": {
 #         "BACKEND": "django_prometheus.cache.backends.redis.RedisCache",
@@ -133,7 +133,7 @@ REDIS_CONN_POOL_1 = redis.ConnectionPool(host=os.environ.get("redisHost"), port=
 #
 # # Redis Settings
 # REDIS_CONN_POOL_1 = redis.ConnectionPool(host='localhost', port=6379, db=0, password='cjgxdulfer8RK51adfJI87sW+CYAXn+nUS3X/oAMeRjUw3gJSBPRkD5LvxbXMiL4nJIfe5KOa0fJ6gAO')
-#
+
 
 PROMETHEUS_METRICS_EXPORT_PORT = 8002
 PROMETHEUS_METRICS_EXPORT_ADDRESS = ''
@@ -191,42 +191,4 @@ ELASTICSEARCH_DSL={
     'default': {
         'hosts': 'localhost:9200'
     },
-}
-
-LOGGING = {
-  'version': 1,
-  'disable_existing_loggers': False,
-  'formatters': {
-      'simple': {
-            'format': '%(asctime)s [%(levelname)s] %(message)s'
-        },
-  },
-  'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'logstash': {
-            'level': 'WARNING',
-            'class': 'logstash.TCPLogstashHandler',
-            'host': 'localhost',
-            'port': 5959, # Default value: 5959
-            'version': 1, # Version of logstash event schema. Default value: 0 (for backward compatibility of the library)
-            'message_type': 'django',  # 'type' field in logstash message. Default value: 'logstash'.
-            'fqdn': False, # Fully qualified domain name. Default value: false.
-            'tags': ['django.request'], # list of tags. Default: None.
-        },
-  },
-  'loggers': {
-        'django.request': {
-            'handlers': ['logstash'],
-            'level': 'WARNING',
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['console'],
-            'propagate': True,
-        },
-    }
 }
