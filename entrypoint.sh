18425c02c676ca45ec85693d1c5f4da66e667395
iampaavan <gopalareddy.p@husky.neu.edu>
2020-03-12 12:27:06 -0400

    refine enterypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd recipe_management
-exec python manage.py migrate
-exec gunicorn recipe_management.wsgi:application --bind 0.0.0.0:8000
+exec python manage.py migrate && gunicorn recipe_management.wsgi:application --bind 0.0.0.0:8000
